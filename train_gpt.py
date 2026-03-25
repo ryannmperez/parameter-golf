1a75031313603b19ab547bb5c9e0305f134c0d26
Ryper1 <105455396+Ryper1@users.noreply.github.com>
2026-03-25 00:19:29 -0400

    Revert "exp011: increase model_dim from 512 to 576"
    This reverts commit 2a3121df4824636bf277bad6722ad64f3fbb73db.

--- train_gpt.py
+++ train_gpt.py
@@ -63,7 +63,7 @@ class Hyperparameters:
     vocab_size = int(os.environ.get("VOCAB_SIZE", 1024))
     num_layers = int(os.environ.get("NUM_LAYERS", 9))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 4))
-    model_dim = int(os.environ.get("MODEL_DIM", 576))
+    model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_heads = int(os.environ.get("NUM_HEADS", 8))
     mlp_mult = int(os.environ.get("MLP_MULT", 3))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "1")))
