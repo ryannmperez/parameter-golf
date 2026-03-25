86e9f53db88f7f9ee6fe4395cd134a264e4c3960
Ryper1 <105455396+Ryper1@users.noreply.github.com>
2026-03-24 23:04:38 -0400

    Revert "exp008: increase depth from 11 to 12 layers"
    This reverts commit aa9975ce9847eb85d74dd3f813c2a15af74daf0c.

--- train_gpt.py
+++ train_gpt.py
@@ -56,7 +56,7 @@ class Hyperparameters:
     max_wallclock_seconds = float(os.environ.get("MAX_WALLCLOCK_SECONDS", 600.0))
     qk_gain_init = float(os.environ.get("QK_GAIN_INIT", 1.5))
     vocab_size = int(os.environ.get("VOCAB_SIZE", 1024))
-    num_layers = int(os.environ.get("NUM_LAYERS", 12))
+    num_layers = int(os.environ.get("NUM_LAYERS", 11))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 4))
     model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_heads = int(os.environ.get("NUM_HEADS", 8))
