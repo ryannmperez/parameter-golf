5fdd573c0e13eb448c8d4cf5da6ce0ff54ce1e18
Ryper1 <105455396+Ryper1@users.noreply.github.com>
2026-03-25 04:48:48 -0400

    Revert "exp023: shorten Muon momentum warmup from 500 to 50 steps"
    This reverts commit fc611a3e11216b90096b02454b96630607514cad.

--- train_gpt.py
+++ train_gpt.py
@@ -80,7 +80,7 @@ class Hyperparameters:
     muon_momentum = float(os.environ.get("MUON_MOMENTUM", 0.99))
     muon_backend_steps = int(os.environ.get("MUON_BACKEND_STEPS", 5))
     muon_momentum_warmup_start = float(os.environ.get("MUON_MOMENTUM_WARMUP_START", 0.85))
-    muon_momentum_warmup_steps = int(os.environ.get("MUON_MOMENTUM_WARMUP_STEPS", 50))
+    muon_momentum_warmup_steps = int(os.environ.get("MUON_MOMENTUM_WARMUP_STEPS", 500))
     beta1 = float(os.environ.get("BETA1", 0.9))
     beta2 = float(os.environ.get("BETA2", 0.95))
     adam_eps = float(os.environ.get("ADAM_EPS", 1e-8))
